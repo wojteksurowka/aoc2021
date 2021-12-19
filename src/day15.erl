e09f1618324ff25e14f805d732f406ab51da952f
Wojtek Surowka <wojtek.surowka@thectgrid.com>
2021-12-19 02:24:42 +0000

    Day 15 polished

--- src/day15.erl
+++ src/day15.erl
@@ -21,48 +21,46 @@ part2() ->
 do_map(Map) ->
     Width = tuple_size(element(1, Map)),
     Height = tuple_size(Map),
-    {Unvisited, BimapInfinity} = lists:foldl(fun (X, Acc) ->
-        lists:foldl(fun (Y, {UnVisitedAcc, BimapAcc}) ->
-            {sets:add_element({X, Y}, UnVisitedAcc), distance_bimap_infinity({X, Y}, BimapAcc)}
-        end, Acc, lists:seq(1, Height))
-    end, {sets:new(), distance_bimap_new()}, lists:seq(1, Width)),
-    InitialBimap = distance_bimap_set({1, 1}, 0, BimapInfinity),
-    distance_bimap_get({Width, Height}, dijkstra({1, 1}, Unvisited, InitialBimap, Map)).
+    Start = {1, 1},
+    End = {Width, Height},
+    InitialBimap = distance_bimap_set(Start, 0, distance_bimap_new(Width, Height)),
+    distance_bimap_get(End, dijkstra(Start, End, InitialBimap, Map)).
 
-dijkstra(Current, Unvisited, Bimap, Map) ->
-    Width = tuple_size(element(1, Map)),
-    Height = tuple_size(Map),
-    UnvisitedNeighbours = lists:filter(fun (N) -> sets:is_element(N, Unvisited) end, neighbours(Current, Map)),
+dijkstra(Current, End, Bimap, Map) ->
+    UnvisitedNeighbours = lists:filter(fun (N) ->
+        not distance_bimap_is_visited(N, Bimap)
+    end, neighbours(Current, Map)),
     CurrentDistance = distance_bimap_get(Current, Bimap),
-    UpdatedBimap = lists:foldl(fun (N, Acc) ->
+    BimapWithNeighbours = lists:foldl(fun (N, Acc) ->
         NeighbourDistance = CurrentDistance + el(N, Map),
         case NeighbourDistance < distance_bimap_get(N, Acc) of
             true -> distance_bimap_set(N, NeighbourDistance, Acc);
             false -> Acc
         end
     end, Bimap, UnvisitedNeighbours),
-    UpdatedUnvisited = sets:del_element(Current, Unvisited),
-    case sets:is_element({Width, Height}, UpdatedUnvisited) of
-        true ->
-            BimapVisited = distance_bimap_set_visited(Current, UpdatedBimap),
-            dijkstra(distance_bimap_min_unvisited(BimapVisited), UpdatedUnvisited, BimapVisited, Map);
-        false ->
-            UpdatedBimap
+    UpdatedBimap = distance_bimap_set_visited(Current, BimapWithNeighbours),
+    case distance_bimap_is_visited(End, UpdatedBimap) of
+        true -> UpdatedBimap;
+        false -> dijkstra(distance_bimap_min_unvisited(UpdatedBimap), End, UpdatedBimap, Map)
     end.
 
 neighbours({X, Y}, Map) ->
     Width = tuple_size(element(1, Map)),
     Height = tuple_size(Map),
-    All = [{X - 1, Y}, {X, Y - 1}, {X + 1, Y}, {X, Y + 1}],
     lists:filter(fun ({XF, YF}) ->
         XF >= 1 andalso XF =< Width andalso YF >= 1 andalso YF =< Height
-    end, All).
+    end, [{X - 1, Y}, {X, Y - 1}, {X + 1, Y}, {X, Y + 1}]).
 
-distance_bimap_new() ->
-    {#{}, #{}, #{}}.
-
-distance_bimap_infinity(XY, {XY2D, D2UXYs, D2VXYs}) ->
-    {XY2D#{XY => infinity}, D2UXYs#{infinity => sets:add_element(XY, maps:get(infinity, D2UXYs, sets:new()))}, D2VXYs}.
+distance_bimap_new(Width, Height) ->
+    lists:foldl(fun (X, XAcc) ->
+        lists:foldl(fun (Y, {XY2DAcc, D2UXYAcc, #{}}) ->
+            {
+                XY2DAcc#{{X, Y} => infinity},
+                D2UXYAcc#{infinity => sets:add_element({X, Y}, maps:get(infinity, D2UXYAcc, sets:new()))},
+                #{}
+            }
+        end, XAcc, lists:seq(1, Height))
+    end, {#{}, #{}, #{}}, lists:seq(1, Width)).
 
 distance_bimap_set(XY, Distance, {XY2D, D2UXYs, D2VXYs}) ->
     PrevDistance = maps:get(XY, XY2D),
@@ -80,9 +78,22 @@ distance_bimap_set_visited(XY, {XY2D, D2UXYs, D2VXYs}) ->
     Distance = maps:get(XY, XY2D),
     {XY2D, element(2, remove_from_d2xy(XY, Distance, D2UXYs)), add_to_d2xy(XY, Distance, D2VXYs)}.
 
+distance_bimap_is_visited(XY, {XY2D, _D2UXYs, D2VXYs}) ->
+    Distance = maps:get(XY, XY2D),
+    case maps:get(Distance, D2VXYs, undefined) of
+        undefined -> false;
+        VisitedWithDistance -> sets:is_element(XY, VisitedWithDistance)
+    end.
+
 distance_bimap_min_unvisited({_XY2D, D2UXYs, _D2VXYs}) ->
     Distance = hd(lists:sort(maps:keys(D2UXYs))),
-    hd(sets:to_list(maps:get(Distance, D2UXYs))).
+    random_set_element(maps:get(Distance, D2UXYs)).
+
+random_set_element(Set) ->
+    sets:fold(fun
+        (Element, undefined) -> Element;
+        (_Element, Found) -> Found
+    end, undefined, Set).
 
 remove_from_d2xy(XY, Distance, D2XY) ->
     case maps:get(Distance, D2XY, undefined) of
