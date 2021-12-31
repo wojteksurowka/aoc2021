9fb4aeb6a80bbef976796f0a0a65091f95c467ff
Wojtek Surowka <wojtek.surowka@thectgrid.com>
2021-12-31 17:25:42 +0000

    Day 24

--- src/day24.erl
+++ src/day24.erl
@@ -1,5 +1,5 @@
 -module(day24).
--export([part1/0]).
+-export([part1/0, part2/0]).
 
 input() ->
     lists:map(fun({Instr, Args}) ->
@@ -14,79 +14,51 @@ input() ->
     end, aoc_input:read(?MODULE, "^ *([a-z]+) +([a-z]+(?: +.+)?) *$", [atom, list])).
 
 part1() ->
-    Code = input(),
-    %find(9#88888888888888, optimise(Code)).
-    Optimised = optimise(Code),
-    lists:map(fun (I) ->
-        execute({0, "" ++ [I + $0]}, lists:sublist(Optimised, 1))
-    end, lists:seq(1, 9)).
-    %io:format("~p~n~p ~p~n", [Optimised, length(Code), length(Optimised)]),
-    %execute(create(nona_to_decimal_string(9#00000000000000)), Optimised).
+    maps:get(0, steps(fun erlang:max/2, split(input()), 14)).
 
-nona_to_decimal_string(N) ->
-    [D + 1 || D <- io_lib:format("~14.9.0B", [N])].
+part2() ->
+    maps:get(0, steps(fun erlang:min/2, split(input()), 14)).
 
-find(N, Code) ->
-    case N rem 100000 of
-        0 -> io:format("~f~n", [(9#88888888888888 - N)/9#88888888888888 * 100]);
-        _ -> ok
-    end,
-    case execute(create(nona_to_decimal_string(N)), Code) of
-        0 -> list_to_integer(nona_to_decimal_string(N));
-        _ -> find(N - 1, Code)
-    end.
+steps(Comparator, Codes, 1) ->
+    lists:foldl(fun (I, Acc) ->
+        Result = calculate(0, I, Codes, 1),
+        case maps:get(Result, Acc, undefined) of
+            undefined -> Acc#{Result => I - $0};
+            PR -> Acc#{Result => Comparator(I - $0, PR)}
+        end
+    end, #{}, lists:seq($1, $9));
+steps(Comparator, Codes, N) ->
+    maps:fold(fun (Init, Input, IAcc) ->
+        lists:foldl(fun (I, Acc) ->
+            Next = calculate(Init, I, Codes, N),
+            case maps:get(Next, Acc, undefined) of
+                undefined -> Acc#{Next => (I - $0) + Input * 10};
+                PI -> Acc#{Next => Comparator((I - $0) + Input * 10, PI)}
+            end
+        end, IAcc, lists:seq($1, $9))
+    end, #{}, steps(Comparator, Codes, N - 1)).
 
-optimise(Code) ->
-    Result = lists:reverse(optimise(Code, [])),
-    case length(Result) < length(Code) of
-        true -> optimise(Result);
-        false -> Result
-    end.
+calculate(Init, SingleInput, Codes, Step) ->
+    maps:get(z, execute({#{w => 0, x => 0, y => 0, z => Init}, [SingleInput]}, element(Step, Codes))).
 
-optimise([], Acc) ->
-    Acc;
-optimise([{mul, x, 0}, {add, x, z} | T], Acc) ->
-    optimise([{x1} | T], Acc);
-optimise([{x1}, {mod, x, 26} | T], Acc) ->
-    optimise([{x2} | T], Acc);
-optimise([{add, x, V}, {eql, x, w}, {eql, x, 0} | T], Acc) when is_integer(V) ->
-    optimise([{x3, V} | T], Acc);
-optimise([{mul, y, 0}, {add, y, V1}, {mul, y, x}, {add, y, V2} | T], Acc) when is_integer(V1), is_integer(V2) ->
-    optimise([{y1, V1, V2} | T], Acc);
-optimise([{mul, y, 0}, {add, y, w}, {add, y, V}, {mul, y, x} | T], Acc) when is_integer(V) ->
-    optimise([{y2, V} | T], Acc);
-optimise([{x2}, {'div', z, V1}, {x3, V2} | T], Acc) when is_integer(V1) ->
-    optimise([{xz1, V1, V2} | T], Acc);
-optimise([{xz1, V1, V2}, {y1, 25, 1}, {mul, z, y}, {y2, V3}, {add, z, y} | T], Acc) ->
-    optimise([{xyz1, V1, V2, V3} | T], Acc);
-optimise([{inp, w}, {xyz1, 1, V1, V2} | T], Acc) ->
-    optimise([{ixyz1, V1, V2} | T], Acc);
-optimise([{inp, w}, {xyz1, 26, V1, V2} | T], Acc) ->
-    optimise([{ixyz2, V1, V2} | T], Acc);
-optimise([H | T], Acc) ->
-    optimise(T, [H | Acc]).
-
-create(Input) ->
-    {0, Input}.
+split(Code) ->
+    list_to_tuple(lists:map(fun lists:reverse/1, lists:reverse(lists:foldl(fun
+        ({inp, R}, Acc) -> [[{inp, R}] | Acc];
+        (Instruction, [H | T]) -> [[Instruction | H] | T]
+    end, [], Code)))).
 
 execute({Regs, Inputs}, Code) ->
-    element(1, lists:foldl(fun
-        ({ixyz1, V1, V2}, {RAcc, [H | T]}) -> {ixyz1(H - $0, V1, V2, RAcc), T};
-        ({ixyz2, V1, V2}, {RAcc, [H | T]}) -> {ixyz2(H - $0, V1, V2, RAcc), T}
-    end, {Regs, Inputs}, Code)).
+    {OutputRegs, []} = lists:foldl(fun
+        ({inp, R}, {RAcc, [H | T]}) -> {RAcc#{R := H - $0}, T};
+        ({add, R, V}, {RAcc, IAcc}) -> {call(fun (X, Y) -> X + Y end, RAcc, R, V), IAcc};
+        ({mul, R, V}, {RAcc, IAcc}) -> {call(fun (X, Y) -> X * Y end, RAcc, R, V), IAcc};
+        ({'div', R, V}, {RAcc, IAcc}) -> {call(fun (X, Y) -> X div Y end, RAcc, R, V), IAcc};
+        ({mod, R, V}, {RAcc, IAcc}) -> {call(fun (X, Y) -> X rem Y end, RAcc, R, V), IAcc};
+        ({eql, R, V}, {RAcc, IAcc}) -> {call(fun (X, X) -> 1; (_X, _Y) -> 0 end, RAcc, R, V), IAcc}
+    end, {Regs, Inputs}, Code),
+    OutputRegs.
 
-ixyz1(I, V1, V2, Acc) ->
-    case Acc rem 26 + V1 == I of
-        false ->
-            Acc * 26 + (I + V2);
-        true ->
-            Acc * 25
-    end.
-
-ixyz2(I, V1, V2, Acc) ->
-    case Acc rem 26 + V1 == I of
-        false ->
-            (Acc div 26) * 26 + (I + V2);
-        true ->
-            (Acc div 26) * 25
-    end.
+call(Fun, Regs, R, V) when is_atom(V) ->
+    call(Fun, Regs, R, maps:get(V, Regs));
+call(Fun, Regs, R, V) when is_integer(V) ->
+    Regs#{R := Fun(maps:get(R, Regs), V)}.
